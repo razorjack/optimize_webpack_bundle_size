ea9cfcc051768f7645a4945f6f9d20b42b5c1781
Jacek Galanciak <jacek.galanciak@gmail.com>
2018-07-01 18:25:43 +0200

    Optimize webpack configuration for smaller bundle size

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,3 +1,10 @@
 const { environment } = require('@rails/webpacker')
 
+const webpack = require('webpack')
+
+environment.plugins.prepend(
+  'MomentIgnoreLocales',
+  new webpack.IgnorePlugin(/^\.\/locale$/, /moment$/)
+)
+
 module.exports = environment
